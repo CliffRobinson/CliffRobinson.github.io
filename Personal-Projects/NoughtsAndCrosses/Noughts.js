b0dea88ea6cfccadffd7a470798f82ca986dc05b
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-05-04 17:05:52 +1200

    updated player info bars, styled active player

--- Personal-Projects/NoughtsAndCrosses/Noughts.js
+++ Personal-Projects/NoughtsAndCrosses/Noughts.js
@@ -2,6 +2,9 @@ function initBoard(){
     let board = document.getElementById('board');
     let size = 3;
 
+    let p1name = 'Cliff';
+    let p2name = 'Kytheon Iora';
+
     for (let i = 0; i<size;i++){
         for (let j = 0; j<size;j++){
             board.innerHTML += '<div class="square"></div>';
@@ -9,12 +12,15 @@ function initBoard(){
         }
     }
 
+    document.getElementById('p1').innerHTML = "Player 1: "+p1name+",<br>You're playing Os."
+    document.getElementById('p2').innerHTML = "Player 2: "+p2name+",<br>You're playing Xs."
+
 }
 
 function addSquareEventListeners(){
     let squares = document.getElementsByClassName('square');
     for (let i of squares){
-        i.addEventListener('click',pinkify);
+        i.addEventListener('click',fillSquare);
     }
 }
 
@@ -23,5 +29,17 @@ function pinkify(){
     event.target.classList.toggle('pink');
 }
 
+function fillSquare(){
+    if (noughtPlayerActive){
+        event.target.classList.add('nought');
+        noughtPlayerActive = false;
+    } else {
+        event.target.classList.add('cross');
+        noughtPlayerActive = true;
+    }
+    event.target.removeEventListener('click',fillSquare);
+}
+
+var noughtPlayerActive = true;
 initBoard();
 addSquareEventListeners();
